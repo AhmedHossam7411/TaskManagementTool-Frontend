6a20999810e8f5904b01c2c88e66fb2d38b4a0cf
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-09-12 18:18:16 +0300

    adding tailwind

--- GovernmentTaskManagement.frontend/src/app/app.ts
+++ GovernmentTaskManagement.frontend/src/app/app.ts
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = 'Government Task Management Tool';
+
+imagePath()
+{
+  return '../assets/images/logo.png';
+}
 }
