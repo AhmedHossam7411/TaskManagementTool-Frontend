374c1a4215ab80ecb67e351668771913159460eb
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-09-15 10:46:32 +0300

    feat: integrate registration form with validation in the frontend
    - Added Tailwind CSS and PostCSS for styling.
    - Updated app.html to include the registration component.
    - Modified app.ts to import the registration component.
    - Created a registration form in register.html with email and password fields.
    - Implemented reactive form in register.ts with validation for email and password.
    - Added error messages for invalid email and password inputs.

--- GovernmentTaskManagement.frontend/src/app/app.ts
+++ GovernmentTaskManagement.frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { register } from "./register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- GovernmentTaskManagement.frontend/src/app/register/register.ts
+++ GovernmentTaskManagement.frontend/src/app/register/register.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
 
+export class register {
+  form = new FormGroup({
+    email : new FormControl('',{
+      validators: [Validators.required, Validators.email,],
+    }),
+    password : new FormControl('',{
+      validators: [Validators.required, Validators.minLength(6),],
+    }),
+  });
+  get emailIsInvalid(){
+    return this.form.controls.email.invalid 
+    && this.form.controls.email.touched
+    && this.form.controls.email.dirty;
+  }
+   get passwordIsInvalid(){
+    return this.form.controls.password.invalid 
+    && this.form.controls.password.touched
+    && this.form.controls.password.dirty;
+  }
+  onSubmit() 
+  {
+    console.log(this.form);
+  }
+  
 }
